a655879267fa0b94c83334b9c4232210876e782d
Rylan Gooch <rylangooch@gmail.com>
2016-07-26 09:39:00 +0100

    Challenge 9

--- bookmark_manager/app/models/link.rb
+++ bookmark_manager/app/models/link.rb
@@ -1,6 +1,18 @@
 require 'data_mapper'
+require 'dm-postgres-adapter'
 
 class Link
-:id :title :url
+
+  include DataMapper::Resource
+
+  property :id,     Serial
+  property :title,  String
+  property :url,    String
 
 end
+
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_test")
+
+DataMapper.finalize
+
+DataMapper.auto_upgrade!
