f5018b83f278e337f68505ae78bf46d336415d24
Rylan Gooch <rylangooch@gmail.com>
2016-07-26 10:15:06 +0100

    challenge 10

--- bookmark_manager/app.rb
+++ bookmark_manager/app.rb
@@ -1,10 +1,11 @@
 require 'sinatra/base'
+require './app/models/link'
 
 class BookmarkManager < Sinatra::Base
-  get '/' do
-    'Hello BookmarkManager!'
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
   end
-
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
