2bc25aa21a3e8875ba4d4be554fdd60cf9914bd6
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-07-26 10:49:57 +0100

    creating add links

--- app/my_app.rb
+++ app/my_app.rb
@@ -2,15 +2,19 @@ require 'sinatra/base'
 require_relative './models/link'
 
 class MyApp < Sinatra::Base
-  # get '/' do
-  #   'Hello MyApp!'
-  # end
 
   get '/links' do
     @links = Link.all
     erb :'links/index'
   end
 
-  # start the server if ruby file executed directly
+  get '/links/new' do
+    erb :'links/new'
+  end
+
+  post '/links' do
+    redirect 'links'
+  end
+  
   run! if app_file == $0
 end
