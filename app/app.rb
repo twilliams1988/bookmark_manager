e6352ea19b74fa4db23052a59bfdbe5ae30afca2
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-07-27 14:26:21 +0100

    Changing file name

---
+++ app/app.rb
@@ -0,0 +1,35 @@
+ENV['RACK_ENV'] ||= 'development'
+
+require 'sinatra/base'
+require_relative 'models/data_mapper_setup'
+
+class BookmarkManager < Sinatra::Base
+
+  get '/' do
+    redirect 'links/new'
+  end
+
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
+  end
+
+  get '/links/new' do
+    erb :'links/new'
+  end
+
+  post '/links' do
+    link = Link.create(url: params[:url], title: params[:title])
+    params[:tags].split.each { |tag| link.tags << Tag.create(name: tag) }
+    link.save
+    redirect '/links'
+  end
+
+  get "/tags/:name" do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :'links/index'
+  end
+
+  run! if app_file == $0
+end
