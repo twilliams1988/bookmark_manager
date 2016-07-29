bbe41279e11a8004b07477c6996332ffa133387c
Tommy Williams <tom@teow.co.uk>
2016-07-29 11:07:57 +0100

    Navigate by tags in address bar

--- app.rb
+++ app.rb
@@ -13,12 +13,18 @@ class BookmarkManager < Sinatra::Base
                 title: params[:title])
   tag  = Tag.first_or_create(name: params[:tags])
   link.tags << tag
-  link.save                   
+  link.save
   redirect to('/links')
   end
 
   get '/links' do
     @links = Link.all
+    erb :'links/index'
+  end
+
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
     erb :'links/index'
   end
 
